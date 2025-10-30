f198366a5fabefe398a9a637b669575689401650
ClaudiaHug <claudia.huguet@dsm-firmenich.com>
2025-10-30 12:25:41 +0100

    changes test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-//import logo from './logo.svg';
+import logo from './dsm_firmenich_Logo.jpg';
 import './App.css';
-
 
 function MyButton() {
   return (
-    <button>
+    <button
+      style={{
+        padding: '10px 20px',
+        fontSize: '16px',
+        cursor: 'pointer',
+        marginTop: '20px',
+      }}
+    >
       Click here
     </button>
   );
@@ -12,8 +18,22 @@ function MyButton() {
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to my app</h1>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <img
+        src={logo}
+        alt="Logo"
+        style={{ width: '250px', marginBottom: '30px' }}
+      />
+      <h1 style={{ fontSize: '20px', marginBottom: '10px'}}>Welcome to my app</h1>
       <MyButton />
     </div>
   );
